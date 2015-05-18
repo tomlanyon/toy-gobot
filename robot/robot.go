869781e01b9e483e106b7719a97c462f5edefd3d
Tom Lanyon <tom@netspot.com.au>
2015-05-18 17:09:51 +0930

    Implemented Left,Right,Move,Report and validate position on the table.

--- robot/robot.go
+++ robot/robot.go
@@ -51,13 +51,66 @@ func NewRobot(minX, maxX, minY, maxY Position) *Robot {
 	}
 }
 
-func (r *Robot) Place(x, y Position, f Facing) {
+func (r *Robot) Place(x, y Position, f Facing) error {
+	if err := r.ValidateBounds(x, y); err != nil {
+		return err
+	}
+
 	r.X = x
 	r.Y = y
 	r.F = f
+
+	return nil
 }
 
-func (r *Robot) Move() {
+func (r *Robot) Left() {
+	if r.F == North {
+		r.F = West
+	} else {
+		r.F--
+	}
+}
+
+func (r *Robot) Right() {
+	if r.F == West {
+		r.F = North
+	} else {
+		r.F++
+	}
+}
+
+func (r *Robot) Move() error {
+	newX, newY := r.X, r.Y
+	switch r.F {
+	case North:
+		newY++
+	case East:
+		newX++
+	case South:
+		newY--
+	case West:
+		newX--
+	}
+
+	if err := r.ValidateBounds(newX, newY); err != nil {
+		return err
+	}
+
+	r.X = newX
+	r.Y = newY
+	return nil
+}
+
+func (r *Robot) ValidateBounds(x,y Position) error {
+	if r.minX <= x && x <= r.maxX &&
+	   r.minY <= y && y <= r.maxY {
+		return nil
+	}
+	return fmt.Errorf("Position %v,%v out of bounds", x, y)
+}
+
+func (r *Robot) Report() {
+	fmt.Printf("%v,%v,%v\n", r.X, r.Y, r.F)
 }
 
 func (r *Robot) Dump() {
