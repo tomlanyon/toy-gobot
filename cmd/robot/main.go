ff1954998bc49a634c3f759f0d280aa28c75108d
Tom Lanyon <tom@netspot.com.au>
2015-05-18 17:14:21 +0930

    Test new functionality in "robot" cmd tool.

--- cmd/robot/main.go
+++ cmd/robot/main.go
@@ -1,8 +1,48 @@
 package main
 
+import "os"
+import "fmt"
 import "github.com/tomlanyon/toy-gobot/robot"
 
 func main() {
 	r := robot.NewRobot(0,5,0,5)
-	r.Dump()
+
+	err := r.Place(1,1,robot.South)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	err = r.Move()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	r.Left()
+
+	err = r.Move()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	err = r.Move()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	r.Report()
+
+	r.Right()
+
+	err = r.Move()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	r.Report()
+
 }
